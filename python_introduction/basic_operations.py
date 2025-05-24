93fa70dfbda8cf0ee5536dc449abc57a32da1ace
132kibsdanger <kibirangokennan01@gmail.com>
2025-05-24 17:10:33 +0300

    Completed the basic_operations.py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+
+#Addition
+y = number1+number2
+
+#Subtraction
+x = number1 - number2
+
+#Multiplication
+z = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {y}")
+print(f"Subtraction of {number1} and {number2} is {x}")
+print(f"Multiplication of {number1} and {number2} is {z}")
