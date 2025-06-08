541336cad8f1b8a319ccec47440788fcd651fa37
132kibsdanger <kibirangokennan01@gmail.com>
2025-06-08 20:43:00 +0300

    Refactored and fixed somethings

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,6 @@
 def perform_operation(num1,num2,operation):
     if operation == 'add':
         result = num1 + num2
-        return result
     elif operation == 'subtract':
         result = num1 - num2
     elif operation == 'multiply':
