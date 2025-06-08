bc4c5faa6d01cc8dc3aa99af0e89da752239699c
132kibsdanger <kibirangokennan01@gmail.com>
2025-06-08 20:53:07 +0300

    More changes to meet alx checker

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,8 +7,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         result = num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            result = num1 / num2
-        else:
+        if num2 == 0:
             result = "Error: Division by zero is not allowed."
+        else:
+            result = num1 / num2
     return result
