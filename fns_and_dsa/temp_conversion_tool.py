f1c5f91b54ae0be440b7be3b2b5d234f5f4fa4b9
132kibsdanger <kibirangokennan01@gmail.com>
2025-06-09 00:14:21 +0300

    Added the global keywords

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
-global FAHRENHEIT_TO_CELSIUS_FACTOR
+
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-global CELSIUS_TO_FAHRENHEIT_FACTOR
+
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
@@ -11,6 +11,7 @@ def convert_to_celsius(fahrenheit):
     :param fahrenheit: Temperature in Fahrenheit
     :return: Temperature in Celsius
     """
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
@@ -20,6 +21,7 @@ def convert_to_fahrenheit(celsius):
     :param celsius: Temperature in Celsius
     :return: Temperature in Fahrenheit
     """
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
